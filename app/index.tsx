5dba30099147f7485fb564807dd324fc49c54a98
Finn1226 <yifeili1226@gmail.com>
2025-09-03 23:06:29 -0400

    added the chords screen to index

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,13 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function Home() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={{ fontSize: 24, marginBottom: 12 }}>Welcome</Text>
+      <Link href="./chords" style={{ fontSize: 18 }}>
+        Go to Chord Finder →
+      </Link>
     </View>
   );
 }
